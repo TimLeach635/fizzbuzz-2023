deb399d41c79003950c22c897e860bac4b335ea9
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2023-10-10 13:30:14 +0100

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 105; i++)
+        {
+            string result = "";
+
+            if (IsDivisibleBy(i, 3))
+            {
+                result += "Fizz";
+            }
+
+            if (IsDivisibleBy(i, 5))
+            {
+                result += "Buzz";
+            }
+
+            if (IsDivisibleBy(i, 7))
+            {
+                result += "Bang";
+            }
+
+            if (result == "")
+            {
+                result = i.ToString();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+
+    private static bool IsDivisibleBy(int numerator, int denominator)
+    {
+        return numerator % denominator == 0;
+    }
+}
